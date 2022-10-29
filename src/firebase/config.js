94a9414d3a94845fec5ea85ff7465c9f5bdd10bb
christopher4RY <arkarlin486@gmail.com>
2022-10-29 13:35:22 +0630

    project setup & firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzWRKOujblA9vIdPiZYUAsJPYZ7CK5Pns",
+  authDomain: "vue-blog-system-2c146.firebaseapp.com",
+  projectId: "vue-blog-system-2c146",
+  storageBucket: "vue-blog-system-2c146.appspot.com",
+  messagingSenderId: "413563081636",
+  appId: "1:413563081636:web:565846c6e1cd91ce36a731",
+};
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+let timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db, timestamp };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+  <div class="containe">
+    <div class="d-flex align-items-center justify-content-center vh-100">
+      <div class="card">
+        <div class="card-body">
+          <h1>Welcome To Live Chat App</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
